fb9f8102953d33858ba3f0b3ddb0ad3985b87ae9
Ivan_Kovinskyi <kovinskyi.ivan@pdffiller.team>
2018-12-26 16:44:18 +0200

    basic nginx site constructor

--- scripts/nginx_host_install.sh
+++ scripts/nginx_host_install.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 MYDIR=$(dirname "${BASH_SOURCE[0]}");
+NGINX_SITE_ENABLED='/usr/local/etc/nginx/servers';
 
-defaultPort='80';
-port=${1:-$defaultPort};
+server=${1:-'default.loc'};
+port=${2:-'80'};
+entry_point=${3:-'public'};
 
-defaultServer='default.loc';
-server=${2:-$defaultServer};
+if [ ! -d $PWD/nginx ]; then
+    mkdir $PWD/nginx;
+fi
+
+NGINX_F=$PWD/nginx/$server;
+
+if [ ! -d $NGINX_F ]; then
+    mkdir $NGINX_F;
+fi
+
+access_log=${4:-$NGINX_F};
+errors_log=${5:-$NGINX_F};
+
+cp $MYDIR/../patterns/nginx/nginx_default.conf.example  $NGINX_F/$server.conf     \
+    && sed -i '' "s|__PORT__|$port|g"                    "$MYDIR/../$server.conf" \
+    && sed -i '' "s|__DOMEN__|$server|g"                 "$MYDIR/../$server.conf" \
+    && sed -i '' "s|__ENTRY_POINT_PATH__|$entry_point|g" "$MYDIR/../$server.conf" \
+    && sed -i '' "s|__ACCESS_LOG_PATH__|$access_log|g"   "$MYDIR/../$server.conf" \
+    && sed -i '' "s|__ERRORS_LOG_PATH__|$errors_log|g"   "$MYDIR/../$server.conf" \
+    && ln -sfn $NGINX_F/$server.conf $NGINX_SITE_ENABLED/$server.conf             \
+;
