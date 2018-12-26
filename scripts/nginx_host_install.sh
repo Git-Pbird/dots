eedcbab8d7c094fb5373b761d88b84d81e09ad13
Ivan_Kovinskyi <kovinskyi.ivan@pdffiller.team>
2018-12-26 16:45:53 +0200

    fix typo

--- scripts/nginx_host_install.sh
+++ scripts/nginx_host_install.sh
@@ -6,24 +6,24 @@ server=${1:-'default.loc'};
 port=${2:-'80'};
 entry_point=${3:-'public'};
 
-if [ ! -d $PWD/nginx ]; then
+if [[ ! -d $PWD/nginx ]]; then
     mkdir $PWD/nginx;
 fi
 
-NGINX_F=$PWD/nginx/$server;
+NGINX_F=$PWD/nginx/${server};
 
-if [ ! -d $NGINX_F ]; then
-    mkdir $NGINX_F;
+if [[ ! -d ${NGINX_F} ]]; then
+    mkdir ${NGINX_F};
 fi
 
 access_log=${4:-$NGINX_F};
 errors_log=${5:-$NGINX_F};
 
-cp $MYDIR/../patterns/nginx/nginx_default.conf.example  $NGINX_F/$server.conf     \
+cp $MYDIR/../patterns/nginx/nginx_default.conf.example  ${NGINX_F}/${server}.conf     \
     && sed -i '' "s|__PORT__|$port|g"                    "$MYDIR/../$server.conf" \
     && sed -i '' "s|__DOMEN__|$server|g"                 "$MYDIR/../$server.conf" \
     && sed -i '' "s|__ENTRY_POINT_PATH__|$entry_point|g" "$MYDIR/../$server.conf" \
     && sed -i '' "s|__ACCESS_LOG_PATH__|$access_log|g"   "$MYDIR/../$server.conf" \
     && sed -i '' "s|__ERRORS_LOG_PATH__|$errors_log|g"   "$MYDIR/../$server.conf" \
-    && ln -sfn $NGINX_F/$server.conf $NGINX_SITE_ENABLED/$server.conf             \
+    && ln -sfn ${NGINX_F}/${server}.conf ${NGINX_SITE_ENABLED}/${server}.conf             \
 ;
