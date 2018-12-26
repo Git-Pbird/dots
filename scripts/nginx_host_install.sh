671e739c30185a79a1d90286705dfa787fdf6b2c
Ivan_Kovinskyi <kovinskyi.ivan@pdffiller.team>
2018-12-26 17:21:14 +0200

    [nginx] adjust install-script, create symlink with default params

--- scripts/nginx_host_install.sh
+++ scripts/nginx_host_install.sh
@@ -19,11 +19,14 @@ fi
 access_log=${4:-$NGINX_F};
 errors_log=${5:-$NGINX_F};
 
-cp $MYDIR/../patterns/nginx/nginx_default.conf.example  ${NGINX_F}/${server}.conf     \
-    && sed -i '' "s|__PORT__|$port|g"                    "$MYDIR/../$server.conf" \
-    && sed -i '' "s|__DOMEN__|$server|g"                 "$MYDIR/../$server.conf" \
-    && sed -i '' "s|__ENTRY_POINT_PATH__|$entry_point|g" "$MYDIR/../$server.conf" \
-    && sed -i '' "s|__ACCESS_LOG_PATH__|$access_log|g"   "$MYDIR/../$server.conf" \
-    && sed -i '' "s|__ERRORS_LOG_PATH__|$errors_log|g"   "$MYDIR/../$server.conf" \
+cp $MYDIR/../patterns/nginx/nginx_default.conf.example   ${NGINX_F}/${server}.conf     \
+    && sed -i '' "s|__PORT__|$port|g"                    ${NGINX_F}/${server}.conf \
+    && sed -i '' "s|__DOMEN__|$server|g"                 ${NGINX_F}/${server}.conf \
+    && sed -i '' "s|__ENTRY_POINT_PATH__|$entry_point|g" ${NGINX_F}/${server}.conf \
+    && sed -i '' "s|__ACCESS_LOG_PATH__|$access_log|g"   ${NGINX_F}/${server}.conf \
+    && sed -i '' "s|__ERRORS_LOG_PATH__|$errors_log|g"   ${NGINX_F}/${server}.conf \
     && ln -sfn ${NGINX_F}/${server}.conf ${NGINX_SITE_ENABLED}/${server}.conf             \
 ;
+echo "You should add new site into '/etc/hosts' manually, or just run command bellow : \n";
+#echo "127.0.0.1:${port} ${server}"  |   sudo tee -a /etc/hosts
+echo '>>> 127.0.0.1:'${port} ${server}'|sudo tee -a /etc/hosts';
